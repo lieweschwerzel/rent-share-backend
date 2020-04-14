baa20eeadf9e8426da36cc9ddc7a8d71b4462a08
Liewe <lieweschwerzel@gmail.com>
2020-04-14 17:35:46 +0200

    Update UsersResource.java

--- src/main/java/com/rentshare/db/resource/UsersResource.java
+++ src/main/java/com/rentshare/db/resource/UsersResource.java
@@ -4,8 +4,11 @@ import com.rentshare.db.model.Users;
 import com.rentshare.db.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+
+//Dit is de controller
 
 @RestController
 @RequestMapping(value = "/rest/users")
@@ -25,19 +28,21 @@ public class UsersResource {
     }
 
 
-    @RequestMapping(method = RequestMethod.POST, value = "/load")
-    public void persist(@RequestBody Users user) {
-        usersRepository.save(user);
-    }
-
-//    @PostMapping(value = "/load")
-//    @ResponseStatus(value = HttpStatus.OK)
-//    public List<Users> persist(@RequestBody final Users users) {
-//        usersRepository.save(users);
-//        if users
-////        return null;
-//        return usersRepository.findAll();
+//    @RequestMapping(method = RequestMethod.POST, value = "/load")
+//    public void persist(@RequestBody Users user) {
+//        usersRepository.save(user);
 //    }
+
+    @PostMapping(value = "/load")
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<Users> persist(@RequestBody final Users users) {
+        usersRepository.save(users);
+//        if (users.equals(users.getName())){
+//            return null;
+//        }
+
+        return usersRepository.findAll();
+    }
 
     @GetMapping(value = "/delete")
     public List<Users> deleteAll() {
@@ -52,25 +57,8 @@ public class UsersResource {
     }
 
 
-//    @GetMapping
-//    public Page<Users> findCustomersByFirstName(
-//            @RequestParam("name") String name,
-//            Pageable pageable) {
-//
-//        if (name == null) {
-//            return usersRepository.findAll(pageable);
-//        } else {
-//            return usersRepository.findByName(name, pageable);
-//        }
-//    }
-
-    @GetMapping(value = "/check/{name}")
-    public List<Users> check(@PathVariable("name") String name) {
-        System.out.println(name);
-
-//        List<Users> users = ;
-
+    @GetMapping(value = "/search/{name}")
+    public List<Users> search(@PathVariable("name") String name) {
         return usersRepository.findByName(name);
-
     }
 }
